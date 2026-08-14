3c308288656f584e9ffef19325f4f330cdb0e57e
Vakarux12 <staneviciusvakaris@gmail.com>
2026-08-14 21:49:03 +0000

    chore: release android 1.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "lt.oranges.orangchat"
         minSdk = 31
         targetSdk = 35
-        versionCode = 74
-        versionName = "1.0.3"
+        versionCode = 75
+        versionName = "1.0.4"
         buildConfigField("String", "KLIPY_API_KEY", "\"$klipyApiKey\"")
         vectorDrawables { useSupportLibrary = true }
     }
